f217b0bb44eecb0f9c59a10c544626d76b6812c0
Buck Perley <bucko.perley@gmail.com>
2021-08-15 20:15:49 -0500

    update default configs

--- components/constants.ts
+++ components/constants.ts
@@ -1,7 +1,7 @@
 export const BOLTWALL_CONFIGS = {
   BOLTWALL_HOST:
-    process.env.NEXT_PUBLIC_BOLTWALL_HOST || 'https://safron-city.vercel.app/',
+    process.env.NEXT_PUBLIC_BOLTWALL_HOST || 'https://caladan.vercel.app/',
   BOLTWALL_RATE: +(process.env.NEXT_PUBLIC_BOLTWALL_RATE || 10), // sats per second
-  BOLTWALL_MIN_TIME: +(process.env.NEXT_PUBLIC_MIN_TIME || 15),
-  NETWORK: process.env.NEXT_PUBLIC_NETWORK || 'testnet',
+  BOLTWALL_MIN_TIME: +(process.env.NEXT_PUBLIC_MIN_TIME || 30),
+  NETWORK: process.env.NEXT_PUBLIC_NETWORK || 'mainnet',
 }
